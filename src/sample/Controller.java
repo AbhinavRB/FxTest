5653a59dc663692b4f9ad1225c04eb6735105d3c
abhinavrb94 <abhinavrb94@gmail.com>
2018-11-30 23:20:54 -0800

    Videos playing with Play/Pause and Seek
    Implemented a Play/Pause button, as well as a seek bar.
    
    To do: make this video player a callable module for replication.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -6,6 +6,7 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Slider;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.util.Duration;
@@ -21,13 +22,31 @@ public class Controller implements Initializable {
 
     @FXML
     private ImageView view1;
+    @FXML
+    private Slider seek1;
 
     private Main main;
     private Image image;
+    private boolean isPlaying;
+    private Timeline timeline;
+    public int currentFrame;
     private String prefix = "C:\\Users\\abhin\\Desktop\\USC Stuff\\CSCI 576 Multimedia Systems\\Final Project\\Data\\USC\\USC\\USCOne\\";
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        isPlaying = false;
+        currentFrame = 1;
+
+        seek1.valueProperty().addListener((observable, oldValue, newValue) -> {
+//            double position = Math.floor((double) newValue) * 33.33;
+            timeline.pause();
+            currentFrame = (int) Math.floor((Double) newValue);
+            if (currentFrame > 8999)
+                currentFrame = 8999;
+            System.out.println(currentFrame);
+            timeline.play();
+        });
+
         try {
             videoLoop();
         } catch (InterruptedException e) {
@@ -45,21 +64,41 @@ public class Controller implements Initializable {
 //            TimeUnit.MILLISECONDS.sleep((long) 33.33);
 //        }
 
-        Timeline timeline = new Timeline(new KeyFrame(Duration.millis(33.33), new EventHandler<ActionEvent>() {
+        timeline = new Timeline(new KeyFrame(Duration.millis(33.33333), new EventHandler<ActionEvent>() {
 
             private int i = 1;
             String pf2 = "USCOne", imageName = "";
 
             @Override
             public void handle(ActionEvent event) {
-                imageName = prefix + pf2 + String.format("%04d", i) + ".rgb";
+                imageName = prefix + pf2 + String.format("%04d", currentFrame) + ".rgb";
 //                System.out.println(imageName);
                 image = main.getImage(imageName);
                 view1.setImage(image);
+                currentFrame++;
+                if (currentFrame >= 8999)
+                    timeline.stop();
                 i++;
             }
         }));
+        isPlaying = true;
         timeline.setCycleCount(9000);
         timeline.play();
+
+        System.out.println("Um, is this after?");
     }
+
+    @FXML
+    public void playPauseToggle() {
+        if (isPlaying) {
+            timeline.pause();
+            isPlaying = false;
+        }
+        else {
+            timeline.play();
+            isPlaying = true;
+        }
+    }
+
+
 }
